1dbca21d78927595c483877456359af8f4dc5dda
papapin777 <papapin777@gmail.com>
2019-06-18 13:45:44 -0700

    39 manipulating arrays with methods

---
+++ L6/notes.js
@@ -0,0 +1,27 @@
+notes = ['Note1', 'Note2', 'Note3', 'Note4'];
+
+console.log(notes);
+
+notes.push('Note5')
+
+console.log(notes);
+
+notes.pop();
+
+console.log(notes);
+
+notes.shift();
+
+console.log(notes);
+
+notes.unshift('Note1');
+
+console.log(notes);
+
+notes.splice(1,1);
+
+console.log(notes);
+
+notes.splice(1, 1, 'NoteLike3')
+
+console.log(notes);

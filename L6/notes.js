d13b1647ce650669c4eb929720d05025a6125841
papapin777 <papapin777@gmail.com>
2019-06-18 13:52:20 -0700

    40 looping over array

--- L6/notes.js
+++ L6/notes.js
@@ -1,27 +1,6 @@
 notes = ['Note1', 'Note2', 'Note3', 'Note4'];
 
-console.log(notes);
-
-notes.push('Note5')
-
-console.log(notes);
-
-notes.pop();
-
-console.log(notes);
-
-notes.shift();
-
-console.log(notes);
-
-notes.unshift('Note1');
-
-console.log(notes);
-
-notes.splice(1,1);
-
-console.log(notes);
-
-notes.splice(1, 1, 'NoteLike3')
-
-console.log(notes);
+notes.forEach(( note, index )=> {
+    index = index + 1;
+    console.log(index + ':' + note)
+})

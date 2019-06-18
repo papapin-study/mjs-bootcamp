b35190584935cf6327bcaa39e8662432c353d661
papapin777 <papapin777@gmail.com>
2019-06-18 14:02:11 -0700

    41 index of method

--- L6/notes.js
+++ L6/notes.js
@@ -1,6 +1,9 @@
 notes = ['Note1', 'Note2', 'Note3', 'Note4'];
 
-notes.forEach(( note, index )=> {
-    index = index + 1;
-    console.log(index + ':' + note)
-})
+c = notes.indexOf('Note1');
+
+console.log(c);
+
+c = notes.indexOf('Note5');
+
+console.log(c);

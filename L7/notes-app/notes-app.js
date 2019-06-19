04b2d3a37a20c69e9731c7d3bea0acfd8656f2a0
papapin <papapin777#gmail.com>
2019-06-18 20:19:01 -0700

    50 dome manipulation

--- L7/notes-app/notes-app.js
+++ L7/notes-app/notes-app.js
@@ -1,1 +1,11 @@
-console.log('cccc')
+
+// const p = document.querySelector('p');
+// p.remove()
+//console.log(p);
+
+const ps = document.querySelectorAll('p')
+ps.forEach( element => {
+    element.textContent = '************'
+    //element.remove();
+    //console.log(element.textContent)
+});
